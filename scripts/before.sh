d09e3c1a56c2edb403afba86ed640e9325190f09
volo-tech <142794535+volo-tech@users.noreply.github.com>
2026-02-10 12:18:13 +0530

    Update before.sh

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
+set -e
+
 echo "Cleaning old deployment..."
-shopt -s dotglob
-rm -rf /home/ubuntu/finnova-backend-frontend-staging/*
-shopt -u dotglob
+rm -rf /home/ubuntu/finnova-backend-frontend-staging/* || true
 
 echo "Updating apt..."
-sudo apt-get update -y
-
-echo "Installing Node.js 20..."
-curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
-sudo -E bash nodesource_setup.sh
-sudo apt-get install -y nodejs
+apt-get update -y
